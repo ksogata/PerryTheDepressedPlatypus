bdb3718a1ee613f2be2920d811212423d622ef45
Kyung Lee <Kyung@wpp-macbook-pro-20160627-4.local>
2019-03-23 23:48:54 -0700

    get all data response format changed

--- index.js
+++ index.js
@@ -33,16 +33,23 @@ var config = {
 firebase.initializeApp(config);
 var firebaseRef = firebase.database();
 
+//GET all the event data.
 app.get('/api/getAllData', async (req, res) => {
   var final = [];
   const eventref = firebaseRef.ref('events');
+  var tempArray = [];
   eventref.on('value', function(snapshot){
     for(var key in snapshot.val()){
-      //console.log(snapshot.val()[i]);
-      var tempArray = [];
-      var jsonO = {};
-      jsonO[key] = snapshot.val()[key];
-      tempArray.push(jsonO);
+      //var jsonO = {};
+      //jsonO[key] = snapshot.val()[key];
+      tempArray.push(snapshot.val()[key]);
+
+      if (tempArray.length == 3) {
+        final.push(tempArray);
+        tempArray = [];
+      }
+    }
+    if (tempArray.length != 0) {
       final.push(tempArray);
     }
     res.json({"result":final});
@@ -51,6 +58,7 @@ app.get('/api/getAllData', async (req, res) => {
   });
 });
 
+//GET all search queries that start with the searched string. EX. Search string = Ba. Will return all events starting with "Ba".
 app.get('/api/getSearchResult', async (req, res) => {
   const eventref = firebaseRef.ref('/events').orderByChild('name').startAt(req.body.name).endAt(req.body.name+'\uF7FF');
   var final = [];
