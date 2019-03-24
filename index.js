4542ede5708ee41108136a7b9803dd9671b363c8
Simon Wu <simon.wu@kp.org>
2019-03-23 19:27:04 -0700

    init firebase

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const path = require('path');
 const https = require('https');
 const request = require('request');
 var Promise = require('promise');
+var firebase = require("firebase");
 
 const app = express();
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCjdIT-abgL0KxsPLUP3f7n3GtefLNw3G4",
+  authDomain: "ulparakatest.firebaseapp.com",
+  databaseURL: "https://ulparakatest.firebaseio.com",
+  storageBucket: "ulparakatest.appspot.com",
+};
+firebase.initializeApp(config);
+console.log("Firebase initialized");
 
 app.get('*', (req, res) => {
 	console.log("App running");
