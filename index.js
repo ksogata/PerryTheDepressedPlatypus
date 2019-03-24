012e41142b185286d765de1267ccecf46598e944
Simon Wu <simon.wu@kp.org>
2019-03-24 09:40:18 -0700

    hotfix #2 for cors

--- index.js
+++ index.js
@@ -20,14 +20,8 @@ var db = admin.firestore();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors())
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
+app.options('*', cors());
 
 // Initialize Firebase
 var config = {
