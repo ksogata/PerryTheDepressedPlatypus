955f6484364b9922aa96b437f5d130e92d453e61
Kyung Lee <Kyung@wpp-macbook-pro-20160627-4.local>
2019-03-23 18:17:50 -0700

    added dummy data getUsers api

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ var Promise = require('promise');
 
 const app = express();
 
+var users = ["Kyung Lee", "Simon Wu", "Subura Pyegnwe", "Uma Last", "Annie Last", "Kevin Saratoga"];
+
+app.get('/api/getUsers', (req, res) => {
+  res.json({"sucess": users});
+
+});
+
 app.get('*', (req, res) => {
 	console.log("App running");
   res.json({'success': 'no errors'});
@@ -14,3 +21,6 @@ app.get('*', (req, res) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port);
+
+
+//first name last name
