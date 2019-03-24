821f6305fe27345576b499b03f2976d049558db6
Kyung Lee <Kyung@wpp-macbook-pro-20160627-4.local>
2019-03-24 00:37:19 -0700

    fixed params head bug.

--- index.js
+++ index.js
@@ -60,7 +60,8 @@ app.get('/api/getAllData', async (req, res) => {
 
 //GET all search queries that start with the searched string. EX. Search string = Ba. Will return all events starting with "Ba".
 app.get('/api/getSearchResult', async (req, res) => {
-  const eventref = firebaseRef.ref('/events').orderByChild('name').startAt(req.params.name).endAt(req.params.name+'\uF7FF');
+  console.log(req.query.name);
+  const eventref = firebaseRef.ref('/events').orderByChild('name').startAt(req.query.name).endAt(req.query.name+'\uF7FF');
   var final = [];
   eventref.on('value', function(snapshot){
     //console.log(snapshot.val());
