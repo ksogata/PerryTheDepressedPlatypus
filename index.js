d38df365fb85ee9672f1095146de7cc48e853c00
Simon Wu <simon.wu@kp.org>
2019-03-24 09:36:35 -0700

    hotfix for cors

--- index.js
+++ index.js
@@ -22,6 +22,13 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors())
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyCjdIT-abgL0KxsPLUP3f7n3GtefLNw3G4",
