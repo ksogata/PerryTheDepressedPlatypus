d5477d66acf8e4cec23245af829e38ade3c9c61a
Simon Wu <simon.wu@kp.org>
2019-03-24 09:57:03 -0700

    fix eventtags in post

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ function postEvent(experience_name, desc, tags, capacity, addr1, addr2, city, st
 app.post('/api/event', (req, res) => {
   try {
   console.log(req.body.tags);
-  postEvent(req.body.experience_name, req.body.desc, req.body.tags, req.body.capacity, req.body.addr1, req.body.addr2, req.body.city, req.body.state, req.body.zip);
+  postEvent(req.body.experience_name, req.body.desc, req.body.eventtags, req.body.capacity, req.body.addr1, req.body.addr2, req.body.city, req.body.state, req.body.zip);
   res.header('Content-Type', 'application/json');
   res.json({"Message": "Success import"});
   }
